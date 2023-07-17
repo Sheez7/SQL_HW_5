7725c14ab8c5024aa7d23e2eaa612df252cb3da8
Sheez7 <sheez2904@gmail.com>
2023-07-17 20:31:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import psycopg2
 
 # 1. Функция, создающая структуру БД (таблицы).
-
 def create_table(cur):
     cur.execute("""
     CREATE TABLE IF NOT EXISTS Client(
@@ -22,11 +21,7 @@ def create_table(cur):
     return "Succesfully created"
 
 # 2. Функция, позволяющая добавить нового клиента.
-
-def add_client(cur):
-    name = input("Input name: ")
-    surname = input("Input surname: ")
-    email = input("Input email: ")
+def add_client(cur, name, surname, email):
     cur.execute("""
     INSERT INTO Client(name, surname, email) VALUES(%s, %s, %s);
     """, (name, surname, email))
@@ -35,64 +30,52 @@ def add_client(cur):
     return "New client not added"
 
 # 3. Функция, позволяющая добавить телефон для существующего клиента.
-
-def add_phone(cur):
-    client = int(input("Input client_id: "))
-    phone = int(input("Input phone_id: "))
-    phone_number = int(input("Input phone_number: "))
+def add_phone(cur, client_id, phone_number):
     cur.execute("""
     INSERT INTO Phone(client_id, phone) VALUES(%s, %s);
-    """, (client, phone_number))
+    """, (client_id, phone_number))
     if cur.rowcount > 0:
        return f"Phone {phone_number} is exists now"
     return "New phone not added"
 
 # 4. Функция, позволяющая изменить данные о клиенте.
-
-def update_client(cur):
-    name = input('Name: ')
-    surname = input('Surname: ')
-    email = input('Email: ')
-    phone = int(input('Phone: '))
-    id = int(input('Phone_id: '))
-    client_id = int(input('Client_id: '))
-    cur.execute("""
-        UPDATE Client SET name=%s, surname=%s, email=%s WHERE id=%s;
-        """, (name, surname, email, id))
-    cur.execute("""
-            UPDATE Phone SET phone=%s WHERE client_id=%s;
-            """, (phone, id, ))
+def update_client(cur, client_id, name=None, surname=None, email=None):
+    if name:
+        cur.execute("""
+            UPDATE Client SET name=%s WHERE id=%s;
+            """, (name, client_id))
+    if surname:
+        cur.execute("""
+            UPDATE Client SET surname=%s WHERE id=%s;
+            """, (surname, client_id))
+    if email:
+        cur.execute("""
+            UPDATE Client SET email=%s WHERE id=%s;
+            """, (email, client_id))
     return cur.rowcount
 
 # 5. Функция, позволяющая удалить телефон для существующего клиента.
-
-def delete_phone(cur):
-    id = int(input('For delete please insert phone id: '))
+def delete_phone(cur, phone_id):
     cur.execute("""
         DELETE FROM Phone WHERE id=%s;
-        """, (id, ))
+        """, (phone_id, ))
     return cur.rowcount
 
 # 6. Функция, позволяющая удалить существующего клиента.
-
-def delete_client(cur):
-    id = int(input('For delete please insert client id: '))
+def delete_client(cur, client_id):
     cur.execute("""
         DELETE FROM Client WHERE id=%s;
-        """, (id, ))
+        """, (client_id, ))
     return cur.rowcount
 
 # 7. Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
-
-def search_client(cur):
-    name = input('Please insert name for search: ')
-    surname = input('Please insert surname for search: ')
-    email = input('Please insert email for search: ')
-    phone = input('Please insert phone for search: ')
-    cur.execute("""
-        SELECT id FROM Client WHERE name=%s AND surname=%s AND email=%s;
-        """, (name, surname, email))
-    cur.execute("""
+def search_client(cur, name=None, surname=None, email=None, phone=None):
+    if name and surname and email:
+        cur.execute("""
+            SELECT id FROM Client WHERE name=%s AND surname=%s AND email=%s;
+            """, (name, surname, email))
+    elif phone:
+        cur.execute("""
             SELECT client_id FROM Phone WHERE phone=%s;
             """, (phone, ))
     if cur.rowcount > 0:
@@ -106,17 +89,20 @@ if __name__  == '__main__':
     with psycopg2.connect(database="SQL_HW_5", user="postgres", password="1") as conn:
         with conn.cursor() as cur:
             create_table(cur)
-            add_client(cur)
-            add_phone(cur)
-            update_client(cur)
-            delete_phone(cur)
-            delete_client(cur)
-            search_client(cur)
-
-
-
-
-
-
-
-
+            name = input("Input name: ")
+            surname = input("Input surname: ")
+            email = input("Input email: ")
+            add_client(cur, name, surname, email)
+            client_id = int(input("Input client_id: "))
+            phone_number = int(input("Input phone_number: "))
+            add_phone(cur, client_id, phone_number)
+            update_client(cur, client_id)
+            phone_id = int(input("Input phone_id: "))
+            delete_phone(cur, phone_id)
+            client_id = int(input("Input client_id: "))
+            delete_client(cur, client_id)
+            name = input('Please insert name for search: ')
+            surname = input('Please insert surname for search: ')
+            email = input('Please insert email for search: ')
+            phone = input('Please insert phone for search: ')
+            search_client(cur, name, surname, email, phone)
